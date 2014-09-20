0b39579f906b24a9c040ddb4d55cda7f5b3b46a4
Scottie Enriquez <scottie@scott-enriquez.net>
2014-09-20 11:11:56 -0500

    Added problem 2

---
+++ 1-15/2.py
@@ -0,0 +1,20 @@
+import sys
+
+MAX_NUMBER = 50000
+MAX_FIBONACCI = 4000000
+#assert: no Fibonacci number can equal -1
+cache = [-1] * MAX_NUMBER
+#set initial values
+cache[1] = 1
+cache[2] = 2
+#account for first even Fibonacci number
+sumOfEvens = 2
+
+for number in range(3, MAX_NUMBER):
+	cache[number] =  cache[number - 2] + cache[number - 1]
+	if (cache[number] > MAX_FIBONACCI):
+		break
+	if (cache[number] % 2 == 0):
+		sumOfEvens += cache[number]
+
+print sumOfEvens
